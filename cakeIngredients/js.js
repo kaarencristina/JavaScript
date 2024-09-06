c24969c28d4c39c7ce617f38e04a50a707cb16fa
kaarencristina <karen-kaah@hotmail.com>
2024-09-06 17:33:08 -0300

    japascript

---
+++ cakeIngredients/js.js
@@ -0,0 +1,22 @@
+let ingredientes=[
+    'agua',
+    'farinha',
+    'ovo',
+    'acucar',
+    'sal'
+];
+
+/*ingredientes[6]='chocolate'; */ /*evitar add itens no array assim*/
+
+
+
+
+/*ingredientes.push('chocolate'); /*forma correta de add um item novo ao array*/
+
+console.log(ingredientes)
+
+ingredientes.shift();
+
+console.log(ingredientes)
+
+console.log(`total de ingredientes sao: ${ingredientes.length}`);
